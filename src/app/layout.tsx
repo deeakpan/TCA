0e8eef4b4eaf3ff4198860b74c7e6b59dea54f1d
deeakpan <deeakpan123@gmail.com>
2025-06-04 16:30:23 +0100

    changed icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,9 @@ const outfit = Outfit({
 export const metadata: Metadata = {
   title: "Cosmic Nexus",
   description: "Cosmic Nexus is a platform where users can discover their cosmic ancestry, generate and mint unique DNA tokens based on cosmic radiation data, and participate in games and experiences powered by true randomness from space.",
+  icons: {
+    icon: "/favicon.png",
+  },
 };
 
 export default function RootLayout({
